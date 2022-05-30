e9f0cd2eb416283ac66b1f81d38fff2329f97ba0
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-05-29 20:49:57 -0700

    test page

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,9 +1,21 @@
-import { StackContext, Api } from "@serverless-stack/resources";
+import { StackContext, Api, ViteStaticSite } from "@serverless-stack/resources";
 
 export function MyStack({ stack }: StackContext) {
-  new Api(stack, "api", {
+  const api = new Api(stack, "api", {
     routes: {
       "GET /": "functions/lambda.handler",
     },
+  });
+
+  const site = new ViteStaticSite(stack, "site", {
+    path: "frontend",
+    environment: {
+      VITE_API_URL: api.url,
+    },
+  });
+
+  stack.addOutputs({
+    apiUrl: api.url,
+    siteUrl: site.url,
   });
 }
