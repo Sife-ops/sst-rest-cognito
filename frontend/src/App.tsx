3fedbc1440cb2b961f50bb6e35fa19121b520395
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-05-29 22:00:04 -0700

    fetch test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  console.log(import.meta.env.VITE_API_URL);
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    fetch(apiUrl).then(async (res) => {
+      const text = await res.text();
+      console.log(text);
+    });
+  }, []);
 
   return (
     <div className="App">
