9db7d9edb87809ff81aa0738843dd856d55ed827
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-05-31 12:31:46 -0700

    extract accountId

--- backend/functions/private.ts
+++ backend/functions/private.ts
@@ -1,9 +1,14 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  const accountId = event.requestContext.accountId;
+
   return {
     statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: 'private route',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      message: "private route",
+      event,
+    }),
   };
 };
