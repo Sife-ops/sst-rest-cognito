9666928fb4fc9cb522c7d92471eb8a29d30369b4
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-05-30 19:21:37 -0700

    form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,41 @@
-// import React from "react";
 // import logo from "./logo.svg";
-
+// import { env } from "./constant";
 import "./App.css";
-import { env } from "./constant";
+import { useState } from "react";
 
 function App() {
-  console.log(env.apiGateway.REGION);
-  console.log(env.apiGateway.URL);
+  // console.log(env.apiGateway.REGION);
+  // console.log(env.apiGateway.URL);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="App">
       {/* // */}
-      hello
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(email);
+          console.log(password);
+        }}
+      >
+        <input
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+        />
+        <br />
+        <input
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+        />
+        <br />
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
