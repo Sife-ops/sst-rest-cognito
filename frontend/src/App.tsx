b19ddcdfaed37c5894410087dfd18375ca59f575
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-05-30 18:09:23 -0700

    install cra

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,12 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import { env } from "./constant";
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  useEffect(() => {
-    fetch(env.apiGateway.URL).then(async (res) => {
-      const text = await res.text();
-      console.log(text);
-    });
-  }, []);
-
   return (
     <div className="App">
       {/* // */}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log("ree");
-        }}
-      >
-        <input
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          value={email}
-        />
-        <br />
-        <input
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          value={password}
-        />
-        <br />
-        <button type="submit">submit</button>
-      </form>
+      hello
     </div>
   );
 }
