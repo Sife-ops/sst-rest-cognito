0eab55db991fe3f40e8bc5e941d169db41f46be2
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-05-31 21:02:07 -0700

    create bookmark

--- backend/functions/private.ts
+++ backend/functions/private.ts
@@ -1,44 +1,130 @@
+import crypto from "crypto";
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { DynamoDB } from "aws-sdk";
 
 const dynamodb = new DynamoDB.DocumentClient();
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
-  // todo: validate
+  const accountId = event.requestContext.accountId;
+  console.log(accountId);
+
   if (!event.body) {
     throw new Error("no body");
   }
+
   const body = JSON.parse(event.body) as {
     operation: string;
-    variables?: Record<string, any>;
-  };
-
-  // const accountId = event.requestContext.accountId;
-  const accountId = "01";
-
-  const response = (body: any) => {
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
-    };
   };
 
   switch (body.operation) {
-    case "item-list":
+    /**
+     * item list
+     */
+    case "item-list": {
       const res = await dynamodb
         .query({
-          // todo: remove bang
+          // todo: remove the bang
           TableName: process.env.tableName!,
           KeyConditionExpression: "pk = :user",
           ExpressionAttributeValues: {
             ":user": `user:${accountId}`,
           },
         })
-        .promise();
-      return response(res.Items);
+        .promise()
+        .then((e) => {
+          console.log(e);
+          return e;
+        });
 
+      return jsonResponse(res.Items);
+    }
+
+    /**
+     * category create
+     */
+    case "category-create": {
+      const { description, name } = getVars<{
+        name?: string;
+        description?: string;
+      }>(body);
+
+      if (!name) {
+        throw new Error("invalid arguments");
+      }
+
+      // todo: prevent duplicate
+
+      const res = await dynamodb
+        .put({
+          TableName: process.env.tableName!,
+          Item: {
+            pk: `user:${accountId}`,
+            sk: `category:${crypto.randomUUID()}`,
+            name,
+            description,
+          },
+        })
+        .promise()
+        .then((e) => {
+          console.log(e);
+          return e;
+        });
+
+      return jsonResponse(res);
+    }
+
+    /**
+     * bookmark create
+     */
+    case "bookmark-create": {
+      const { name, description, url, favorite } = getVars<{
+        name?: string;
+        description?: string;
+        url?: string;
+        favorite?: boolean;
+      }>(body);
+
+      if (!name) {
+        throw new Error("invalid arguments");
+      }
+
+      const res = await dynamodb
+        .put({
+          TableName: process.env.tableName!,
+          Item: {
+            pk: `user:${accountId}`,
+            sk: `bookmark:${crypto.randomUUID()}`,
+            name,
+            description,
+            url,
+            favorite,
+          },
+        })
+        .promise()
+        .then((e) => {
+          console.log(e);
+          return e;
+        });
+
+      return jsonResponse(res);
+    }
+
+    /**
+     * default
+     */
     default:
       throw new Error("unknown operation");
   }
+};
+
+const getVars = <T>(body: any): T => {
+  return body.variables as T;
+};
+
+const jsonResponse = (body: any) => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  };
 };
--- frontend/src/component/dev.tsx
+++ frontend/src/component/dev.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { API, Auth } from "aws-amplify";
 
 export const Dev: React.FC = () => {
+  const [name, setName] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [url, setUrl] = React.useState("");
+  const [favorite, setFavorite] = React.useState(false);
+
   React.useEffect(() => {
     Auth.currentAuthenticatedUser().then((e) => {
       console.log(e);
@@ -10,7 +15,85 @@ export const Dev: React.FC = () => {
 
   return (
     <div className="App">
-      <h1>api</h1>
+      <h1>create bookmark</h1>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const res = await API.post("temp", "/private", {
+            body: {
+              operation: "bookmark-create",
+              variables: {
+                name,
+                description,
+                url,
+                favorite,
+              },
+            },
+          });
+          console.log(res);
+        }}
+      >
+        <input
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <br />
+        <input
+          placeholder="description"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+        <br />
+        <input
+          placeholder="url"
+          onChange={(e) => setUrl(e.target.value)}
+          value={url}
+        />
+        <br />
+        <label>favorite</label>
+        <input
+          type="checkbox"
+          placeholder="favorite"
+          onChange={(e) => setFavorite((s) => !s)}
+          checked={favorite}
+        />
+        <br />
+        <button type="submit">submit</button>
+      </form>
+
+      <h1>create category</h1>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const res = await API.post("temp", "/private", {
+            body: {
+              operation: "category-create",
+              variables: {
+                name,
+                description,
+              },
+            },
+          });
+          console.log(res);
+        }}
+      >
+        <input
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <br />
+        <input
+          placeholder="description"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+        <br />
+        <button type="submit">submit</button>
+      </form>
+
+      <h1>get</h1>
       <button
         onClick={async () => {
           const res = await API.get("temp", "/public", {});
@@ -21,6 +104,7 @@ export const Dev: React.FC = () => {
       </button>
       <br />
 
+      <h1>list</h1>
       <button
         onClick={async () => {
           const res = await API.post("temp", "/private", {
@@ -31,7 +115,7 @@ export const Dev: React.FC = () => {
           console.log(res);
         }}
       >
-        private
+        list
       </button>
     </div>
   );
