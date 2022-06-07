6ace6f35e987a1f1635809e37093df3f1d68021f
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-06-06 21:27:01 -0700

    refactor bookmark-list

--- backend/lib/operation/bookmark-list.ts
+++ backend/lib/operation/bookmark-list.ts
@@ -1,61 +1,42 @@
-import { Ok, Err } from "ts-results";
-import { OperationFn } from "./operation";
-import { db } from "../service";
+import model from '../model';
+import { Ok, Err } from 'ts-results';
+import { OperationFn } from './operation';
+import { db } from '../service';
 
-const bookmarkList: OperationFn<{}, "bookmarks undefined"> = async ({
+const bookmarkList: OperationFn<{}, 'bookmarks undefined'> = async ({
   accountId,
 }) => {
-  const bookmarksRes = await db
-    .query({
-      TableName: process.env.tableName!,
-      KeyConditionExpression: "pk = :pk and begins_with(sk, :sk)",
-      ExpressionAttributeValues: {
-        ":pk": `user:${accountId}`,
-        ":sk": `bookmark:`,
-      },
-    })
-    .promise()
-    .then((e) => {
-      console.log(e);
-      return e;
-    });
+  const bookmarksRes = await model
+    .query('pk')
+    .eq(`user:${accountId}`)
+    .where('sk')
+    .beginsWith('bookmark')
+    .exec();
 
-  const categoriesRes = await db
-    .query({
-      TableName: process.env.tableName!,
-      KeyConditionExpression: "pk = :pk and begins_with(sk, :sk)",
-      ExpressionAttributeValues: {
-        ":pk": `user:${accountId}`,
-        ":sk": `category:`,
-      },
-    })
-    .promise()
-    .then((e) => {
-      console.log(e);
-      return e;
-    });
+  const categoriesRes = await model
+    .query('pk')
+    .eq(`user:${accountId}`)
+    .where('sk')
+    .beginsWith('category')
+    .exec();
 
-  const bookmarks = bookmarksRes.Items?.map((bookmark) => {
-    const categories = categoriesRes.Items?.reduce(
-      (acc: any[], cur, _, arr) => {
-        if (cur.sk.includes(bookmark.sk)) {
-          const category = arr.find((category) => {
-            return category.sk === cur.sk.split("#")[0];
-          });
-          return [...acc, category];
-        }
-        return acc;
-      },
-      []
-    );
-
+  const bookmarks = bookmarksRes.map((bookmark) => {
+    const categories = categoriesRes.reduce((acc: any[], cur, _, arr) => {
+      if (cur.sk.includes(bookmark.sk)) {
+        const category = arr.find((category) => {
+          return category.sk === cur.sk.split('#')[0];
+        });
+        return [...acc, category];
+      }
+      return acc;
+    }, []);
     return {
       ...bookmark,
       categories,
     };
   });
 
-  if (!bookmarks) return Err("bookmarks undefined");
+  console.log(bookmarks);
 
   return Ok(bookmarks);
 };
--- backend/lib/operation/item-list.ts
+++ backend/lib/operation/item-list.ts
@@ -1,10 +1,10 @@
-import model from "../model";
-import { Ok } from "ts-results";
-import { OperationFn } from "./operation";
+import model from '../model';
+import { Ok } from 'ts-results';
+import { OperationFn } from './operation';
 
 const itemList: OperationFn = async ({ accountId }) => {
-  const res = await model.query("pk").eq(`user:${accountId}`).exec();
-  return Ok(res.toJSON());
+  const items = await model.query('pk').eq(`user:${accountId}`).exec();
+  return Ok(items);
 };
 
 export default itemList;
